ae854cc20d6bcfcf26c8373edc98fa5cc29ef83c
nguyenhuongvi1712 <huongvinguyen171200@gmail.com>
2024-01-22 19:31:27 +0700

    upload img

--- src/utils/api/index.js
+++ src/utils/api/index.js
@@ -27,6 +27,48 @@ export async function login({ email, password }) {
 }
 
 
+// images
+export async function getListImages({page, limit}) {
+    try {
+		const res = await axiosInstance.get(
+			`uploadImage/get_images?page=${page}&limit=${limit}`,
+		);
+		return {
+			success: true,
+			data: res.data,
+		};
+	} catch (error) {
+		console.log(error);
+		return {
+			success: false,
+			msg: error.response?.data.detail || 'Something was wrong!',
+		};
+	}
+}
 
+
+
+export async function uploadImage(file) {
+	try {
+        const token = localStorage.getItem('token');
+		const formData = new FormData();
+		formData.append('files', file);
+		const res = await axiosInstance.post('uploadImage/upload', formData, {
+			headers: {
+				'Content-Type': 'multipart/form-data',
+				accept: 'application/json',
+				Authorization: `Bearer ${token}`,
+			},
+		});
+		return {
+			success: true,
+			data: res.data,
+		};
+	} catch (error) {
+		return {
+			success: false,
+		};
+	}
+}
 
 
